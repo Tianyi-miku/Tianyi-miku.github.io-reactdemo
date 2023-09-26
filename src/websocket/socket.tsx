47bec5a6f212d9afb5f111fa87fa0578a7d17b03
liguanjie <1249850908@qq.com>
2023-09-26 14:21:55 +0800

    重写websocket

--- src/websocket/socket.tsx
+++ src/websocket/socket.tsx
@@ -1,89 +1,108 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import config from "@/config";
 import React from "react";
+import { generalNotification } from "@/utils/utils";
+
 
 //websocket,打开音频或者频谱图时候调用，关闭页面时清除
-const Socket = () => {
-
-  let websocket: any = React.useRef<WebSocket>();
-  const connectNum = React.useRef<number>(0); // 已重连次数
-  // let websocket: WebSocket;
-  let lockReconnect = false;
-  //
-  const [socketData, setsocketData] = useState('');
-  let createWebSocket = (url: string | URL) => {
-    websocket.current = new WebSocket(url);
-    websocket.current.onopen = function () {
-      connectNum.current = 0;
-      if (config.IsDev) {
-        return;
-      }
-      heartCheck.reset();
-      heartCheck.start();
-    };
-    websocket.current.onerror = function () {
-      reconnect(url);
-    };
-    websocket.current.onclose = function (e: any) {
-      console.log(
-        "websocket 断开: " + e.code + " " + e.reason + " " + e.wasClean
-      );
-    };
-    websocket.current.onmessage = function (event: any) {
-      lockReconnect = true;
-      //event 为服务端传输的消息，在这里可以处理
-      setsocketData(event.data);
-    };
-  };
-
-  let reconnect = (url: string | URL) => {
-    if (lockReconnect) return;
-    //正式环境没连接上会一直重连，设置延迟避免请求过多，本地就不连了
-    if (config.IsDev) {
-      return;
-    }
-    const reconnectCount = config.wsReconnectNum;
-    if (connectNum.current < reconnectCount) {
-      setTimeout(() => {
-        createWebSocket(url);
-        connectNum.current = connectNum.current + 1;
-        console.log(`ws失败重连第${connectNum.current}次`);
-      }, 3000);
-    }
-  };
-
-  let timeoutObj: string | number | NodeJS.Timeout | undefined;
-  let timeout = 60000; //60秒
-  let heartCheck = {
-    reset: function () {
-      clearInterval(timeoutObj);
-      return;
-    },
-    start: function () {
-      timeoutObj = setInterval(function () {
-        //这里发送一个心跳，后端收到后，返回一个心跳消息，
-        //onmessage拿到返回的心跳就说明连接正常
-        websocket.current.send("HeartBeat");
-      }, timeout);
-    },
-  };
-  //关闭连接
-  let closeWebSocket = () => {
-    websocket.current && websocket.current.close();
-  };
-
-  let sendMsg = (params: any) => {
-    websocket.current.send(params)
-  };
-
-  return {
-    createWebSocket,
-    closeWebSocket,
-    socketData,
-    sendMsg,
-    websocket
-  };
+export type SocketOptions = {
+  url: URL | string; // 连接的url地址
+  type: 'situation' | 'single'; // 应用类型
+  onOpen?: Function; // 自定义onopen回调函数
+  onError?: Function; // 自定义onerror回调函数
+  onClose?: Function; // 自定义onclose回调函数
+  onMessage?: Function; // 自定义onmessage回调函数
+  reconnect?: boolean; // 是否开启失败重连
+  reconnectCount?: number; // 最大失败重连次数
+  heartCheck?: boolean; // 是否开启心跳检测
+  showMsg?: boolean; // 是否显示错误提示
 };
 
-export default Socket;
+function useSocket(options: SocketOptions) {
+  options.reconnect = options?.reconnect || true; // 默认开启失败重连
+  options.heartCheck = options?.heartCheck || true; // 默认开启心跳检测
+  let websocket: any = React.useRef<WebSocket>();
+
+  const [socketData, setsocketData] = useState()
+
+
+  const lockReconnect = React.useRef<boolean>(false); // 锁住重连
+  const connectNum = React.useRef<number>(0); // 已重连次数
+  const timeoutObj: any = React.useRef(null); // 心跳检测定时器
+
+  const reconnectFunc = React.useCallback(
+    (callback: Function) => {
+      if (lockReconnect.current) return;
+      const reconnectCount = options.reconnectCount || config.wsReconnectNum;
+      // if (!config.IsDev && connectNum.current < reconnectCount) {
+      if (connectNum.current < reconnectCount) {
+        setTimeout(() => {
+          callback();
+          connectNum.current = connectNum.current + 1;
+          console.log(`ws失败重连第${connectNum.current}次`);
+        }, 3000);
+      }
+    },
+    [lockReconnect, connectNum, options]
+  );
+  const resetHeart = React.useCallback(() => {
+    timeoutObj.current && clearInterval(timeoutObj.current);
+    timeoutObj.current = null;
+  }, [timeoutObj]);
+
+  const startHeart = React.useCallback(() => {
+    timeoutObj.current = setInterval(function () {
+      websocket.current.ws && websocket.current.ws.send("HeartBeat");
+    }, 60000);
+  }, [timeoutObj, websocket]);
+
+  const createWebSocket = React.useCallback(() => {
+    if (!websocket.current) {
+      websocket.current = new WebSocket(options.url);
+      websocket.current.onopen = (res: any) => {
+        console.log(options.url + "连接成功");
+        connectNum.current = 0;
+        options?.onOpen && options.onOpen(res);
+        if (!config.IsDev && options.heartCheck) {
+          resetHeart();
+          startHeart();
+        }
+      };
+      websocket.current.onerror = (err: any) => {
+        options?.onError && options.onError(err);
+        options?.reconnect && reconnectFunc(() => createWebSocket());
+        if (options.showMsg) {
+          generalNotification.error("websocket连接错误");
+        }
+      };
+      websocket.current.onclose = (res: any) => {
+        options?.onClose && options.onClose(res);
+        if (options.showMsg) {
+          generalNotification.error("websocket连接错误");
+        }
+      };
+      websocket.current.onmessage = function (event: any) {
+        setsocketData(event.data)
+      };
+    }
+  },
+    [
+      websocket,
+      options,
+      connectNum,
+      reconnectFunc,
+      resetHeart,
+      startHeart,
+    ]);
+
+  // createWebSocket();
+
+  return React.useMemo<any>(() => ({
+    websocket,
+    socketData,
+    createWebSocket
+  }), [websocket, socketData, createWebSocket]);
+}
+
+export default useSocket;
